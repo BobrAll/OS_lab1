bded7d5bbfba57b62f5ffce6b5c6cffdda69cab3
bobr <bobrus91@gmai.com>
2023-10-30 00:09:42 +0300

    updated

--- subsystems/memory.py
+++ subsystems/memory.py
@@ -3,7 +3,7 @@ import subprocess
 
 def test_memory(time, min_val, max_val):
     x, y = [], []
-    info = {'x_label': '%MEM\'s', 'y_label': 'bogo ops'}
+    info = {'x_label': 'memrate\'s', 'y_label': 'used mem (Mb)'}
     memory_methods = ['memrate']
     method = input('Enter needed memory method from list: {} >>>'.format(memory_methods))
 
@@ -16,21 +16,20 @@ def test_memory(time, min_val, max_val):
         command_to_run = command.format(method, method_val, time)
         print('command:', command_to_run)
 
-        info['title'] = command_to_run
+        info['title'] = command
         info['parameter'] = method
 
         subprocess.Popen(command_to_run, stdout=subprocess.PIPE, shell=True, executable="/bin/bash")
         proc = subprocess.Popen('free -m | tail -2 | head -1', stdout=subprocess.PIPE, shell=True,
                                 executable="/bin/bash")
-        output = str(proc.stdout.read())
+        output = str(proc.stdout.read())[:-3]
 
         digit_counter = 0
         for val in output.split(' '):
             if val.isdigit():
                 print(val)
                 digit_counter += 1
-                if digit_counter == 7:
-                    print("FOUNDED")
+                if digit_counter == 2:
                     x.append(method_val)
                     y.append(int(val))
                     break
