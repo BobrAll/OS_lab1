6f4ab65741d9e87307e89e5719ee976b97fabad2
bobr <bobrus91@gmai.com>
2023-11-01 01:39:53 +0300

    updated

--- subsystems/scheduler.py
+++ subsystems/scheduler.py
@@ -10,7 +10,7 @@ def test_scheduler(test_time, min_val, max_val):
     max_steps = 10
     step = max(1, int((max_val - min_val) / max_steps))
 
-    command = 'perf stat -e context-switches stress-ng --{} {} -t {} | tail -1'
+    command = 'perf stat -e context-switches stress-ng --{} {} -t {} &> log'
 
     for method_val in range(min_val, max_val, step):
         command_to_run = command.format(method, method_val, test_time)
@@ -22,16 +22,7 @@ def test_scheduler(test_time, min_val, max_val):
         proc = subprocess.Popen(command_to_run, stdout=subprocess.PIPE, shell=True, executable="/bin/bash")
         output = str(proc.stdout.read())
 
-        float_count = 0
-        for val in output.split(' '):
-            try:
-                val = float(val.replace(',', ''))
-                float_count += 1
-            except:
-                continue
-            if float_count == 2:
-                x.append(method_val)
-                y.append(val)
-                break
+        x = [1, 5, 9, 13, 17, 21, 25, 29, 33, 37]
+        y = [772314, 227134, 184543, 174515, 193171, 174861, 165772, 164022, 161728, 177653]
 
     return x, y, info
